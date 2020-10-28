b326f5b765efd30d02fa45171c9aa4dd1cd8ceac
Tadas <tadasgailevicius@yahoo.com>
2020-10-28 18:49:11 +0200

    Add MusicServiceConnection.kt class

---
+++ app/src/main/java/com/example/spotifycloneapp/exoplayer/MusicServiceConnection.kt
@@ -0,0 +1,60 @@
+package com.example.spotifycloneapp.exoplayer
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.session.MediaControllerCompat
+import android.support.v4.media.session.PlaybackStateCompat
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.spotifycloneapp.other.Constants.NETWORK_ERROR
+import com.example.spotifycloneapp.other.Event
+import com.example.spotifycloneapp.other.Resource
+
+class MusicServiceConnection(
+    context: Context
+) {
+    private val _isConnected = MutableLiveData<Event<Resource<Boolean>>>()
+    val isConnected: LiveData<Event<Resource<Boolean>>> = _isConnected
+
+    private val _networkError = MutableLiveData<Event<Resource<Boolean>>>()
+    val networkError: LiveData<Event<Resource<Boolean>>> = _networkError
+
+    private val _playbackState = MutableLiveData<PlaybackStateCompat?>()
+    val playbackState: LiveData<PlaybackStateCompat?> = _playbackState
+
+    private val _curPlayingSong = MutableLiveData<MediaMetadataCompat?>()
+    val curPlayingSong: LiveData<MediaMetadataCompat?> = _curPlayingSong
+
+    lateinit var mediaController: MediaControllerCompat
+
+    val transportControls: MediaControllerCompat.TransportControls
+        get() = mediaController.transportControls
+
+    private inner class MediaControllerCallback : MediaControllerCompat.Callback() {
+
+        override fun onPlaybackStateChanged(state: PlaybackStateCompat?) {
+            _playbackState.postValue(state)
+        }
+
+        override fun onMetadataChanged(metadata: MediaMetadataCompat?) {
+            _curPlayingSong.postValue(metadata)
+        }
+
+        override fun onSessionEvent(event: String?, extras: Bundle?) {
+            super.onSessionEvent(event, extras)
+            when(event) {
+                NETWORK_ERROR -> _networkError.postValue(
+                    Event(
+                        Resource.error(
+                            "Couldn't connect to the server. Please check your internet connection.",
+                            null
+                        )
+                    )
+                )
+            }
+        }
+    }
+
+
+}
