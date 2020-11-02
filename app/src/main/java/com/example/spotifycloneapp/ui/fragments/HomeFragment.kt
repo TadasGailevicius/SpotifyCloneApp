02793868d4c29ffea18acf989540c58a227aae0f
Tadas <tadasgailevicius@yahoo.com>
2020-11-02 19:34:41 +0200

    Add lines to HomeFragment.kt class

--- app/src/main/java/com/example/spotifycloneapp/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/spotifycloneapp/ui/fragments/HomeFragment.kt
@@ -1,8 +1,56 @@
 package com.example.spotifycloneapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.spotifycloneapp.R
+import com.example.spotifycloneapp.adapters.SongAdapter
+import com.example.spotifycloneapp.other.Status
+import com.example.spotifycloneapp.ui.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_home.*
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class HomeFragment : Fragment(R.layout.fragment_home) {
-    
+
+    lateinit var mainViewModel: MainViewModel
+
+    @Inject
+    lateinit var songAdapter: SongAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        setupRecyclerView()
+        subscribeToObservers()
+
+        songAdapter.setOnItemClickListener {
+            mainViewModel.playOrToggleSong(it)
+        }
+    }
+
+    private fun setupRecyclerView() = rvAllSongs.apply {
+        adapter = songAdapter
+        layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun subscribeToObservers(){
+        mainViewModel.mediaItems.observe(viewLifecycleOwner){result ->
+            when(result.status){
+                Status.SUCCESS -> {
+                    allSongsProgressBar.isVisible = false
+                    result.data?.let {songs ->
+                        songAdapter.songs = songs
+                    }
+                }
+
+                Status.ERROR -> Unit
+                Status.LOADING -> allSongsProgressBar.isVisible = true
+            }
+        }
+    }
 }
