47ae2fffe088d2464f1652344f54514c5495ba59
Tadas <tadasgailevicius@yahoo.com>
2020-11-02 18:55:28 +0200

    Add HomeFragment.kt class

---
+++ app/src/main/java/com/example/spotifycloneapp/ui/fragments/HomeFragment.kt
@@ -0,0 +1,8 @@
+package com.example.spotifycloneapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.spotifycloneapp.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+    
+}
