f8ba0034374d34df8115f2f995ae13b5ecf3bc60
Tadas <tadasgailevicius@yahoo.com>
2020-11-11 22:17:35 +0200

    Add methods to MainActivity.kt class Fix HomeFragment.kt class

--- app/src/main/java/com/example/spotifycloneapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/spotifycloneapp/ui/MainActivity.kt
@@ -2,18 +2,71 @@ package com.example.spotifycloneapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.bumptech.glide.RequestManager
 import com.example.spotifycloneapp.R
+import com.example.spotifycloneapp.adapters.SwipeSongAdapter
+import com.example.spotifycloneapp.data.entities.Song
+import com.example.spotifycloneapp.other.Status
+import com.example.spotifycloneapp.other.Status.*
+import com.example.spotifycloneapp.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val mainViewModel: MainViewModel by viewModels()
+
+    @Inject
+    lateinit var swipeSongAdapter: SwipeSongAdapter
+
     @Inject
     lateinit var glide: RequestManager
+
+    private var curPlayingSong: Song? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        vpSong.adapter = swipeSongAdapter
+    }
+
+    private fun switchViewPagerToCurrentSong(song: Song) {
+        val newItemIndex = swipeSongAdapter.songs.indexOf(song)
+        if(newItemIndex != -1){
+            vpSong.currentItem = newItemIndex
+            curPlayingSong = song
+        }
+    }
+
+    private fun subscribeToObservers() {
+        mainViewModel.mediaItems.observe(this) {
+            it?.let { result ->
+                when (result.status) {
+                    SUCCESS -> {
+                        result.data?.let { songs ->
+                            swipeSongAdapter.songs = songs
+                            if(songs.isNotEmpty()) {
+                                glide.load((curPlayingSong ?: songs[0]).imageUrl).into(ivCurSongImage)
+                            }
+
+                            switchViewPagerToCurrentSong(curPlayingSong ?: return@observe)
+                        }
+                    }
+                    ERROR -> Unit
+                    LOADING -> Unit
+                }
+            }
+        }
+
+        mainViewModel.curPlayingSong.observe(this){
+            if(it == null) return@observe
+
+            curPlayingSong = it
+        }
+
     }
 }
