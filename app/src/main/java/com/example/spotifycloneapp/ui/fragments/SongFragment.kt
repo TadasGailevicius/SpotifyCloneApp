ce3726606182862a77e5db1865b329ff45fc95ad
Tadas <tadasgailevicius@yahoo.com>
2020-11-12 23:05:29 +0200

    Add SongFragment.kt class

---
+++ app/src/main/java/com/example/spotifycloneapp/ui/fragments/SongFragment.kt
@@ -0,0 +1,8 @@
+package com.example.spotifycloneapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.spotifycloneapp.R
+
+class SongFragment : Fragment(R.layout.fragment_song) {
+
+}
