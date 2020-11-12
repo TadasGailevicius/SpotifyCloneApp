10ad263aeeac038349fc232fe43a0a9a839412d9
Tadas <tadasgailevicius@yahoo.com>
2020-11-12 22:37:49 +0200

    Add SongViewModel.kt class

---
+++ app/src/main/java/com/example/spotifycloneapp/ui/viewmodels/SongViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.spotifycloneapp.ui.viewmodels
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.spotifycloneapp.exoplayer.MusicServiceConnection
+import kotlinx.coroutines.launch
+
+class SongViewModel @ViewModelInject constructor(
+    musicServiceConnection: MusicServiceConnection
+) : ViewModel() {
+
+    private val playbackState = musicServiceConnection.playbackState
+
+    private val _curSongDuration = MutableLiveData<Long>()
+    val curSongDuration: LiveData<Long> = _curSongDuration
+
+    private val _curPlayerPosition = MutableLiveData<Long>()
+    val curPlayerPosition: LiveData<Long> = _curPlayerPosition
+
+    private fun updateCurrentPlayerPosition() {
+        viewModelScope.launch {
+            while (true){
+                
+            }
+        }
+    }
+
+
+}
