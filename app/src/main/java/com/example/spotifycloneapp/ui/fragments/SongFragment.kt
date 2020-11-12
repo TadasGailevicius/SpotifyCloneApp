23d4a23e6d90487071e800ddced05018b83f7a5c
Tadas <tadasgailevicius@yahoo.com>
2020-11-13 00:28:11 +0200

    Update SongFragment.kt class

--- app/src/main/java/com/example/spotifycloneapp/ui/fragments/SongFragment.kt
+++ app/src/main/java/com/example/spotifycloneapp/ui/fragments/SongFragment.kt
@@ -1,19 +1,24 @@
 package com.example.spotifycloneapp.ui.fragments
 
 import android.os.Bundle
+import android.support.v4.media.session.PlaybackStateCompat
 import android.view.View
+import android.widget.SeekBar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.RequestManager
 import com.example.spotifycloneapp.R
 import com.example.spotifycloneapp.data.entities.Song
+import com.example.spotifycloneapp.exoplayer.isPlaying
 import com.example.spotifycloneapp.exoplayer.toSong
 import com.example.spotifycloneapp.other.Status.SUCCESS
 import com.example.spotifycloneapp.ui.viewmodels.MainViewModel
 import com.example.spotifycloneapp.ui.viewmodels.SongViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_song.*
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -27,10 +32,47 @@ class SongFragment : Fragment(R.layout.fragment_song) {
 
     private var curPlayingSong: Song? = null
 
+    private var playbackState: PlaybackStateCompat? = null
+
+    private var shouldUpdateSeekBar = true
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
         subscribeToObservers()
+
+        ivPlayPauseDetail.setOnClickListener{
+            curPlayingSong?.let {
+                mainViewModel.playOrToggleSong(it,true)
+            }
+        }
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if(fromUser){
+                    setCurPlayerTimeToTextView(progress.toLong())
+                }
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                shouldUpdateSeekBar = false
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                seekBar?.let {
+                    mainViewModel.seekTo(it.progress.toLong())
+                    shouldUpdateSeekBar = true
+                }
+            }
+        })
+
+        ivSkipPrevious.setOnClickListener{
+            mainViewModel.skipToPreviousSong()
+        }
+
+        ivSkip.setOnClickListener{
+            mainViewModel.skipToNextSong()
+        }
     }
 
     private fun updateTitleAndSongImage(song: Song) {
@@ -55,10 +97,39 @@ class SongFragment : Fragment(R.layout.fragment_song) {
                 }
             }
         }
+
         mainViewModel.curPlayingSong.observe(viewLifecycleOwner) {
             if(it == null) return@observe
             curPlayingSong = it.toSong()
             updateTitleAndSongImage(curPlayingSong!!)
         }
+
+        mainViewModel.playbackState.observe(viewLifecycleOwner){
+            playbackState = it
+            ivPlayPauseDetail.setImageResource(
+                if(playbackState?.isPlaying == true) R.drawable.ic_pause else R.drawable.ic_play
+            )
+
+            seekBar.progress = it?.position?.toInt() ?: 0
+        }
+
+        songViewModel.curPlayerPosition.observe(viewLifecycleOwner){
+            if(shouldUpdateSeekBar){
+                seekBar.progress = it.toInt()
+                setCurPlayerTimeToTextView(it)
+            }
+        }
+
+        songViewModel.curSongDuration.observe(viewLifecycleOwner){
+            seekBar.max = it.toInt()
+            val dateFormat = SimpleDateFormat("mm:ss", Locale.getDefault())
+            tvSongDuration.text = dateFormat.format(it)
+
+        }
+    }
+
+    private fun setCurPlayerTimeToTextView(ms: Long){
+        val dateFormat = SimpleDateFormat("mm:ss", Locale.getDefault())
+        tvCurTime.text = dateFormat.format(ms)
     }
 }
