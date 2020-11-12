40ad4eb60983154d2bffe5cf8bbcb802c9defc6b
Tadas <tadasgailevicius@yahoo.com>
2020-11-12 23:54:45 +0200

    Add methods to SongFragment.kt class

--- app/src/main/java/com/example/spotifycloneapp/ui/fragments/SongFragment.kt
+++ app/src/main/java/com/example/spotifycloneapp/ui/fragments/SongFragment.kt
@@ -1,8 +1,64 @@
 package com.example.spotifycloneapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.RequestManager
 import com.example.spotifycloneapp.R
+import com.example.spotifycloneapp.data.entities.Song
+import com.example.spotifycloneapp.exoplayer.toSong
+import com.example.spotifycloneapp.other.Status.SUCCESS
+import com.example.spotifycloneapp.ui.viewmodels.MainViewModel
+import com.example.spotifycloneapp.ui.viewmodels.SongViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_song.*
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SongFragment : Fragment(R.layout.fragment_song) {
 
+    @Inject
+    lateinit var glide: RequestManager
+
+    private lateinit var mainViewModel: MainViewModel
+    private val songViewModel: SongViewModel by viewModels()
+
+    private var curPlayingSong: Song? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        subscribeToObservers()
+    }
+
+    private fun updateTitleAndSongImage(song: Song) {
+        val title = "${song.title} - ${song.subtitle}"
+        tvSongName.text = title
+        glide.load(song.imageUrl).into(ivSongImage)
+    }
+
+    private fun subscribeToObservers() {
+        mainViewModel.mediaItems.observe(viewLifecycleOwner) {
+            it?.let { result ->
+                when(result.status) {
+                    SUCCESS -> {
+                        result.data?.let { songs ->
+                            if(curPlayingSong == null && songs.isNotEmpty()) {
+                                curPlayingSong = songs[0]
+                                updateTitleAndSongImage(songs[0])
+                            }
+                        }
+                    }
+                    else -> Unit
+                }
+            }
+        }
+        mainViewModel.curPlayingSong.observe(viewLifecycleOwner) {
+            if(it == null) return@observe
+            curPlayingSong = it.toSong()
+            updateTitleAndSongImage(curPlayingSong!!)
+        }
+    }
 }
