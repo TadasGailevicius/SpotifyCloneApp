2ec37bb63a92cec404999132411ed22b456f1092
Tadas <tadasgailevicius@yahoo.com>
2021-01-05 23:58:46 +0200

    Add User.kt data class

---
+++ app/src/main/java/com/example/spotifycloneapp/data/entities/User.kt
@@ -0,0 +1,16 @@
+package com.example.spotifycloneapp.data.entities
+
+import com.example.spotifycloneapp.other.Constants.DEFAULT_PROFILE_PICTURE_URL
+import com.google.firebase.firestore.Exclude
+import com.google.firebase.firestore.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+data class User (
+        val uid: String = "",
+        val username: String = "",
+        val profilePictureUrl: String = DEFAULT_PROFILE_PICTURE_URL,
+        val description: String = "",
+        var follows: List<String> = listOf(),
+        @get:Exclude
+        var isFollowing: Boolean = false
+)
