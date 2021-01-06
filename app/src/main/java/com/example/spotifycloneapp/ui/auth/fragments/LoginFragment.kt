c3291d2f71c6a02206f79315b1a5490d11aca70f
Tadas <tadasgailevicius@yahoo.com>
2021-01-06 23:25:33 +0200

    Add LoginFragment.kt Fragment

---
+++ app/src/main/java/com/example/spotifycloneapp/ui/auth/fragments/LoginFragment.kt
@@ -0,0 +1,10 @@
+package com.example.spotifycloneapp.ui.auth.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.spotifycloneapp.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginFragment : Fragment(R.layout.fragment_login){
+
+}
