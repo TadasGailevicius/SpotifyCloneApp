ffdc8a272dc6cbabe4c39f8b0db2a06fec075f41
Tadas <tadasgailevicius@yahoo.com>
2021-01-06 20:49:05 +0200

    Add link to DEFAULT_PROFILE_PICTURE_URL in Constants.kt

--- app/src/main/java/com/example/spotifycloneapp/other/Constants.kt
+++ app/src/main/java/com/example/spotifycloneapp/other/Constants.kt
@@ -2,7 +2,7 @@ package com.example.spotifycloneapp.other
 
 object Constants {
 
-    const val DEFAULT_PROFILE_PICTURE_URL = ""
+    const val DEFAULT_PROFILE_PICTURE_URL = "https://firebasestorage.googleapis.com/v0/b/spotifycloneapp-d22c4.appspot.com/o/img_avatar.png?alt=media&token=5481d692-2541-41c1-ab4e-34bea7925fc5"
 
     const val SONG_COLLECTION = "songs"
 
