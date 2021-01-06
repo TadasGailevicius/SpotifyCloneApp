7e10073c11f75de24b7d087c99ef3751567ddd6f
Tadas <tadasgailevicius@yahoo.com>
2021-01-06 23:31:14 +0200

    Add RegisterFragment.kt Fragment

---
+++ app/src/main/java/com/example/spotifycloneapp/ui/auth/fragments/RegisterFragment.kt
@@ -0,0 +1,10 @@
+package com.example.spotifycloneapp.ui.auth.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.spotifycloneapp.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class RegisterFragment : Fragment(R.layout.fragment_register){
+
+}
